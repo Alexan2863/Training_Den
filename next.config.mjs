8e011af6f9f058baa1dc7de57a76f2576d2ae30f
Alex Powell <alexan2863@gmail.com>
2025-12-13 15:20:34 -0700

    Removed webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config, { dev, isServer }) => {
-    if (dev && !isServer) {
-      // Suppress source map warnings from browser extensions (React DevTools, etc.)
-      // These extensions inject scripts with source map references that don't exist
-      config.ignoreWarnings = [
-        ...(config.ignoreWarnings || []),
-        {
-          module: /installHook\.js/,
-          message: /Failed to parse source map/,
-        },
-        {
-          // Ignore all source map warnings for anonymous code (browser extensions)
-          message: /Failed to parse source map.*anonymous/,
-        },
-      ];
-
-      // Also suppress webpack dev server overlay for these warnings
-      if (config.devServer) {
-        config.devServer.client = {
-          ...config.devServer.client,
-          overlay: {
-            errors: true,
-            warnings: false, // Don't show warnings in overlay
-          },
-        };
-      }
-    }
-
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
